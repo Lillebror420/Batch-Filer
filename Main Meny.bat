bb94afb5d3eb113e0f8582ad6cba5449eb275ec8
Lillebror420 <mikael@tommelop.dk>
2022-05-25 23:54:57 +0200

    Update Main Meny.bat

--- Main Meny.bat
+++ Main Meny.bat
@@ -35,7 +35,10 @@ echo 2: User list on computer
 echo 4: Start Notepad
 echo 5: Start Edge
 echo 6: Vis bruger liste
-echo 7: Exit Menu
+echo 7: Viser liste over dine diske
+echo 8: Opret en ny bruger
+echo 9: Slet en bruger
+echo 10: Exit Menu
 echo -----------------------------------------------
 set /p p=:
 if %p%==1 goto :IP
@@ -43,7 +46,10 @@ if %p%==2 goto :Users
 if %p%==4 goto :Notepad
 if %p%==5 goto :edge
 if %p%==6 goto :Users
-if %p%==7 goto :Exit
+if %p%==7 goto :disks
+if %p%==8 goto :userNew
+if %p%==9 goto :userDel
+if %p%==10 goto :Exit
 
 :Notepad
 start Notepad
@@ -63,7 +69,7 @@ cls
 echo ------------------------------------------------
 ipconfig
 echo ------------------------------------------------
-echo a: back to main menu
+echo a: Tilbage til hovede menu
 set /p p=:
 if %p%==a goto :menu
 
@@ -73,6 +79,41 @@ cls
 echo ---------------------------------------------------------------------------------------------------------
 net users
 echo ---------------------------------------------------------------------------------------------------------
-echo a: back to main menu
+echo a: Tilbage til hovede menu
+set /p p=:
+if %p%==a goto :menu
+
+:disks
+title Harddiske
+cls
+echo ---------------------------------------------------------------------------------------------------------
+wmic logicaldisk get caption,providername,drivetype,volumename,description
+echo ---------------------------------------------------------------------------------------------------------
+echo a: Tilbage til hovede menu
+set /p p=:
+if %p%==a goto :menu
+
+:userNew
+cls
+echo ---------------------------------------------------------------------------------------------------------
+title Opret ny bruger
+set /p UNAME="Hvad skal brugeren hedde: "
+set /p PWORD="Hvilket password skal brugeren have: "
+net user %UNAME% %PWORD%% /add
+echo Brugeren %UNAME% er nu oprettet med %PWORD%
+echo ---------------------------------------------------------------------------------------------------------
+echo a: Tilbage til hovede menu
+set /p p=:
+if %p%==a goto :menu
+
+:userDel
+cls
+echo ---------------------------------------------------------------------------------------------------------
+title Slet en bruger
+set /p UNAME="Hvilken bruger skal slettes: "
+net user %UNAME% /del
+echo Brugeren %UNAME% er nu slettet
+echo ---------------------------------------------------------------------------------------------------------
+echo a: Tilbage til hovede menu
 set /p p=:
 if %p%==a goto :menu
